3a2c8dd1c18ba6a0f855c9f16946b118aa8673bd
Jazia ABDELKRIM <107263611+jazia-abdelkrim@users.noreply.github.com>
2025-05-04 02:50:45 +0000

    last

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import PrivacyPolicy from "./PrivacyPolicy.jsx";
-<Route path="/team" element={<Team />} />
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Links from "./Links";
+import Team from "./Team";
 
 function App() {
-    return <Liks />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Links />} />
+        <Route path="/team" element={<Team />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
