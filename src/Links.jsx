2fc075f3a08c62f6dd10152510c542cd12a21ee4
Jazia ABDELKRIM <107263611+jazia-abdelkrim@users.noreply.github.com>
2025-05-04 08:29:33 +0100

    done

--- src/Links.jsx
+++ src/Links.jsx
@@ -29,8 +29,6 @@ export default function AmanahLinktree() {
 
                 {/* website Section */}
                 <a
-                    href=""
-                    target="_blank"
                     className="flex items-center justify-center gap-3 w-full text-center py-3 bg-[#4C87FF] text-white text-xl rounded-lg hover:bg-[#41E2B8] shadow transition"
                 >
                     Visit Our Website (Soon)
